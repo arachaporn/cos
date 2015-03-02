60973ed9e65e80ec3d67df5b24c8b140509bcf19
Arachaporn <arachaporn.su@gmail.com>
2015-03-02 13:59:36 +0700

    no message

--- footer.php
+++
@@ -1,6 +0,0 @@
-<div class="footer">
-© 2014, The Copyright CDIP (Thailand) Co.,Ltd.  All rights reserved.<br>
-131 INC1 No.227 Thailand Science park Paholyothin Rd. Klong1 Klong Luang Pathumthani 12120 THAILAND<br>
-Tel: 0 2564 7000 #5204,0 2564 7200 #5204 Fax: 0 2564 7745
-</div>
-</body>
